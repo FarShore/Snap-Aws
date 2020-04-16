9a99e5d652fb1e47a2b4fcd5f90eb3a20b1bbcb5
jegan <jeganfarshore@gmail.com>
2020-04-16 15:07:44 +0530

    normaltest

--- src/aws/aws.controller.js
+++ src/aws/aws.controller.js
@@ -11,83 +11,9 @@ const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_S3_VIDEO_OVERSIZED_PREFIX = process.env.AWS_S3_VIDEO_OVERSIZED_PREFIX
 const AWS_REGION = process.env.AWS_REGION
 export function uploadToAws(req, res, next) {
-  let file = req.file;
-
-  // Clean the file name of special characters, extra spaces, etc.
-  let fileName = file.originalname
-                          .replace(/[^a-zA-Z0-9. ]/g, '')
-                          .replace(/\s+/g, ' ')
-                          .replace(/[ ]/g, '-');
-
-  // Check if the fileName has a file extension
-  // If it doesn't we add a fileExt from the mimetype
-  let pattern = /\.[0-9a-z]+$/i;
-  let foundFileExt = fileName.match(pattern);
-  if (!foundFileExt) {
-    // Get the file extension from the mimetype
-    let fileExt = file.mimetype.substr(file.mimetype.indexOf('/') + 1);
-
-    // Add to the fileName
-    fileName += `.${fileExt}`;
-  }
-
-  // Create random string to ensure unique filenames
-  let randomBytes = crypto.randomBytes(32).toString('hex');
-
-  /**
-   * Create aws file key by combining random string and file name
-   * e.g., 73557ec94ea744c5c24bdb03ee114a1ef83ab2dd9bfb20f38999faea14564d19/DarthVader.jpg
-   */
-  let fileKey = `${AWS_S3_FILES_KEY_PREFIX}/${randomBytes}/${fileName}`;
-
-
-  // Configure aws
-  aws.config.accessKeyId = AWS_ACCESS_KEY_ID;
-  aws.config.secretAccessKey = AWS_SECRET_ACCESS_KEY;
-
-
-  // Create our bucket and set params
-  // let bucket = new aws.S3({
-  //   params: { Bucket: AWS_S3_FILES_BUCKET }
-  // });
-
-    let bucket = new aws.S3({
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY
-   });
-
-
-  console.log("testing",bucket)
-
-  let params = {
-    
-    Key: fileKey,
-    Body: fs.createReadStream(file.path),
-    Bucket: AWS_S3_FILES_BUCKET,
-    ContentType: file.mimetype != '' ? file.mimetype : 'application/octet-stream'
-  };
-
-  // Upload file to s3
-  bucket.upload(params, function(err, data) {
-    if (data) {
-      // Delete the file once it's been uploaded to s3
-      fs.unlinkSync(file.path);
-
-      let fileObject = {
-        name: fileName,
-        type: file.mimetype,
-        size: file.size,
-        url: fileKey
-      };
-
-      res.status(200).json(fileObject);
-    } else {
-      // Delete the file
-      fs.unlinkSync(file.path);
-      err.jegan = "checeking"
-      res.status(403).json(err);
-    }
-  });
+   let file = req.file;
+    res.status(200).json(file);
+   
 }
 
 export function s3Signature(req, res, next) {
